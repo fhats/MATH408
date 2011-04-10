b218e60a5fd2f560b90999f9e967f0284a753c8d
Fred Hatfull <fxh32@case.edu>
2011-04-10 19:25:40 -0400

    Initial working implementation of the LSB hiding algorithm

--- Algorithms/LSB Hiding/lsb_hiding.py
+++ Algorithms/LSB Hiding/lsb_hiding.py
@@ -1,4 +1,5 @@
 import wave         # Python rocks!
+import struct
 import sys
 
 # Uses LSB hiding to embed a given set of watermark data
@@ -7,23 +8,94 @@ import sys
 # cover_filepath is the path to a host WAV audio file 
 # watermark is the data to be embedded (can be any type, only the binary representation is used)
 # watermarked_output_path is the path for the watermarked audio data to be written to
-def lsb_watermark(cover_filepath, watermark, watermarked_output_path):
-    watermark_end_signal = 0xBEEFBABE
-    watermark_length = sys.getsizeof(watermark)
+def lsb_watermark(cover_filepath, watermark_data, watermarked_output_path):
+    
+    watermark_str = str(watermark_data)
+    watermark = struct.unpack("%dB" % len(watermark_str), watermark_str)
+    
+    watermark_size = len(watermark)
+    watermark_bits = watermark_to_bits((watermark_size,), 32)
+    watermark_bits.extend(watermark_to_bits(watermark))
     
     cover_audio = wave.open(cover_filepath, 'rb') 
-    output = wave.open(watermarked_output_path, 'wb')
     
-    n = 0
+    (nchannels, sampwidth, framerate, nframes, comptype, compname) = cover_audio.getparams()
+    frames = cover_audio.readframes (nframes * nchannels)
+    samples = struct.unpack_from ("%dh" % nframes * nchannels, frames)
+
+    if len(samples) < len(watermark_bits):
+        raise OverflowError("The watermark data provided is too big to fit into the cover audio!") 
+    
+    print "Watermarking %s (%d samples) with %d bits of information." % (cover_filepath, len(samples), len(watermark_bits))
+    
+    encoded_samples = []
+    
     watermark_position = 0
-    while n < cover_audio.getnframes():
-        current_sample = cover_audio.readframes(1)
+    n = 0
+    for sample in samples:
+        encoded_sample = sample
         
-        if watermark_position > watermark_length:
+        if watermark_position < len(watermark_bits):
+            encode_bit = watermark_bits[watermark_position]
+            if encode_bit == 1:
+                encoded_sample = sample | encode_bit
+            else:
+                encoded_sample = sample
+                if sample & 1 != 0:
+                    encoded_sample = sample - 1
+                    
+            watermark_position = watermark_position + 1
             
-        
-        watermark_position = watermark_position + 1
-        n = n + 1
+        encoded_samples.append(encoded_sample)
+            
+    encoded_audio = wave.open(watermarked_output_path, 'wb')
+    encoded_audio.setparams( (nchannels, sampwidth, framerate, nframes, comptype, compname) )
+
+    encoded_audio.writeframes(struct.pack("%dh" % len(encoded_samples), *encoded_samples))
+
+def watermark_to_bits(watermark, nbits=8):
+    watermark_bits = []
+    for byte in watermark:
+        for i in range(0,nbits):
+            watermark_bits.append( (byte & (2 ** i)) >> i )
+    return watermark_bits
+    
+def recover_lsb_watermark(watermarked_filepath):
+    # Simply collect the LSB from each sample
+    watermarked_audio = wave.open(watermarked_filepath, 'rb') 
+    
+    (nchannels, sampwidth, framerate, nframes, comptype, compname) = watermarked_audio.getparams()
+    frames = watermarked_audio.readframes (nframes * nchannels)
+    samples = struct.unpack_from ("%dh" % nframes * nchannels, frames)
+    
+    # determine how many watermark bytes we should look for
+    watermark_bytes = 0
+    for (sample,i) in zip(samples[0:32], range(0,32)):
+        watermark_bytes = watermark_bytes + ( (sample & 1) * (2 ** i))
+    
+    print "Recovering %d bytes of watermark information from %s (%d samples)" % (watermark_bytes, watermarked_filepath, len(samples))
+    
+    watermark_data = []
+    
+    for n in range(0, watermark_bytes):
+        watermark_byte_samples = samples[32 + (n * 8) : 32+((n+1) * 8)]
+        watermark_byte = 0
+        for (sample, i) in zip(watermark_byte_samples, range(0,8)):
+            watermark_byte = watermark_byte + ( (sample & 1) * (2**i) )
+        watermark_data.append(watermark_byte)
+            
+    print struct.pack("%dB" % len(watermark_data), *watermark_data)
     
 if __name__ == "__main__":
-    lsb_watermark("../test_data/bach.wav", "excellent", "w.wav")
+    message = "this is an example of steganography in audio!"
+    cover_audio = "../test_data/bach.wav"
+    output = "w.wav"
+    if len(sys.argv) > 1:
+        message = sys.argv[1]
+        if len(sys.argv) > 2:
+            cover_audio = sys.argv[2]
+            if len(sys.argv) > 3:
+                output = sys.argv[3]
+    lsb_watermark(cover_audio, message, output)
+    
+    recover_lsb_watermark("w.wav")
