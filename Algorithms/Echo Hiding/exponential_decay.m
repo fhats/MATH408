b081d39b26fb44d71a7670213e4035bafedc0ea3
Fred <fxh32@case.edu>
2011-04-12 16:37:27 -0400

    Echo watermarking WIP

--- Algorithms/Echo Hiding/exponential_decay.m
+++ Algorithms/Echo Hiding/exponential_decay.m
@@ -1,6 +1,10 @@
 function [ processed_wave ] = exponential_decay( wav, Fs, time, decay )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+    if time > 10
+        % time is probably in ms... so treat it as such
+        time = time / 1000;
+    end
     processed_wave = wav;
     N = (time)*Fs;
     for n=N+1:length(wav),
@@ -8,4 +12,3 @@ function [ processed_wave ] = exponential_decay( wav, Fs, time, decay )
     end
 
 end
-
