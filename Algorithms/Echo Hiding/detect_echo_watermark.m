1618d49561f71b300f49d9a363db7fda46a4afd6
Fred Hatfull <fxh32@case.edu>
2011-04-27 12:23:59 -0400

    Early smoothing algorithm works. Needs to be tested against more echo delay times.

--- Algorithms/Echo Hiding/detect_echo_watermark.m
+++ Algorithms/Echo Hiding/detect_echo_watermark.m
@@ -13,7 +13,7 @@ function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay
     delay_0_signal = [];
     delay_1_signal = [];
     decision_signal = [];
-    
+
     while (pos+nw <= nx)                       % while enough signal left
             y = encoded_wav(pos:pos+nw-1).*w;           % make window y
             c = abs(rceps(y));
@@ -21,14 +21,23 @@ function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay
             
             delay_0_signal(pos) = c(round(delay_0 * Fs) + 1);
             delay_1_signal(pos) = c(round(delay_1 * Fs) + 1);
-            if delay_1_signal(pos) - delay_0_signal(pos) > 0
-                decision_signal(pos) = 1;
-            else
-                decision_signal(pos) = 0;
-            end
             
             pos = pos + round(nw/50);                 % next window
     end
+    
+    last_recorded_bit = 0;
+    for pos=1:length(delay_0_signal),
+            if delay_1_signal(pos) - delay_0_signal(pos) > 0
+                decision_signal(pos) = 1;
+                last_recorded_bit = 1;
+            elseif delay_1_signal(pos) - delay_0_signal(pos) < 0
+                decision_signal(pos) = 0;
+                last_recorded_bit = 0;
+            else
+                decision_signal(pos) = last_recorded_bit;
+            end        
+    end
+    
     figure(2);
     hold on;
     %axis([-20, length(decision_signal), -0.1, 1.1]);
@@ -39,6 +48,5 @@ function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay
     plot(delay_1_signal);
     figure(5);
     plot(delay_0_signal);
-    
     
 end
