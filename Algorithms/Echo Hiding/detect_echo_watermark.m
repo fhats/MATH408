d15fb3ca8a2a3a61bdc7f799346fc365a1df6d9d
Fred Hatfull <fxh32@case.edu>
2011-04-27 11:21:38 -0400

    Playing with cepstrum differentials. In many cases the difference between cepstrums is 0. This should be investigated and can perhaps be overcome with some kind of smoothing.

--- Algorithms/Echo Hiding/detect_echo_watermark.m
+++ Algorithms/Echo Hiding/detect_echo_watermark.m
@@ -1,17 +1,17 @@
-function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay_1, delay_2 )
+function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay_0, delay_1 )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     % divide up a signal into windows
+    delay_0 = delay_0 / 1000;
     delay_1 = delay_1 / 1000;
-    delay_2 = delay_2 / 1000;
     
     nx = length(encoded_wav);                            % size of signal
     w = hamming(2000);                          % hamming window
     nw = length(w);                            % size of window
     pos=1;
     
+    delay_0_signal = [];
     delay_1_signal = [];
-    delay_2_signal = [];
     decision_signal = [];
     
     while (pos+nw <= nx)                       % while enough signal left
@@ -19,22 +19,26 @@ function [ recovered_watermark ] = detect_echo_watermark( encoded_wav, Fs, delay
             c = abs(rceps(y));
             ac = autoceps(y);
             
+            delay_0_signal(pos) = c(round(delay_0 * Fs) + 1);
             delay_1_signal(pos) = c(round(delay_1 * Fs) + 1);
-            delay_2_signal(pos) = c(round(delay_2 * Fs) + 1);
-            if c(round(delay_1 * Fs)+1) > c(round(delay_2 * Fs)+1)
-                decision_signal(pos) = 0;
-            else
+            if delay_1_signal(pos) - delay_0_signal(pos) > 0
                 decision_signal(pos) = 1;
+            else
+                decision_signal(pos) = 0;
             end
             
             pos = pos + round(nw/50);                 % next window
     end
-
     figure(2);
     hold on;
-    axis([-20, length(decision_signal), -0.1, 1.1]);
+    %axis([-20, length(decision_signal), -0.1, 1.1]);
     plot(decision_signal);
-    
+    figure(3);
+    plot(delay_1_signal - delay_0_signal);
+    figure(4);
+    plot(delay_1_signal);
+    figure(5);
+    plot(delay_0_signal);
     
     
 end
